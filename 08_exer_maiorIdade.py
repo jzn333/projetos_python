992e5ec9bfbfafaad502a31c959dee695e9f9109
jzn333 <144640019+jzn333@users.noreply.github.com>
2023-09-10 22:56:51 -0300

    Add files via upload

---
+++ 08_exer_maiorIdade.py
@@ -0,0 +1,13 @@
+nome = "João Vitor"
+sobrenome = "Silva"
+idade = 18
+anoNascimento = 2023 - idade
+maiorIdade = idade >= 18
+alturaMetros = 1.70
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", anoNascimento)
+print("É maior de idade?", maiorIdade)
+print("Altura em metros:", alturaMetros)
---
+++ 12_exer_calcularIMC.py
@@ -0,0 +1,6 @@
+nome = "João Vitor"
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+print(nome, "tem", altura, "de altura", "pesa", peso, "e o seu imc é:", imc)
